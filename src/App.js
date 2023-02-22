390d5ea5b2873ccfa4a7a92356cb064bc37e46fd
Rodrigo <rodrigodomingospereira99@gmail.com>
2023-02-22 11:31:11 +0000

    git deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import InputBar from './components/InputBar';
+import Task from './components/Task';
+import { useState } from 'react';
+import ModalWindow from './components/ModalWindow';
+import Button from 'react-bootstrap/Button';
 function App() {
+
+  const [toDo, setToDo] = useState([{
+    id: 0,
+    text: 'Learn React',
+    completed: false
+  }, {
+    id: 1,
+    text: 'Buy groceries',
+    completed: true
+  }, {
+    id: 2,
+    text: 'Finish cleaning',
+    completed: false
+  }
+  ]);
+
+  const [hideTasks, setHideTasks] = useState(false)
+
+  const addTask = (taskText) => {
+    setToDo([...toDo, {
+      id: toDo.length + 1,
+      text: taskText,
+      completed: false
+    }])
+  };
+
+  const delTodo = (id) => {
+    const newTasks = toDo.filter(task => task.id !== id)
+    setToDo(newTasks);
+  }
+
+  const updateCompleted = (id) => {
+    const updatedTask = toDo.map(task => {
+      if (task.id === id) {
+        return ({ ...task, completed: !task.completed })
+      }
+      return task;
+    })
+    setToDo(updatedTask);
+  }
+
+  const handleHide = () => {
+    setHideTasks(!hideTasks);
+  }
+
+  const deleteDone = () => {
+    const doneTasks = toDo.filter(task => task.completed === false);
+    setToDo(doneTasks);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>Todo App</h1>
+      <InputBar newTodo={addTask}></InputBar>
+      {!hideTasks && toDo.map((task) => (<Task
+        key={task.id}
+        todo={task}
+        deleteTodo={delTodo}
+        setCompleted={updateCompleted} />))}
+      <Button className="endButtons" onClick={handleHide}>Hide all</Button>
+      <Button className="endButtons" onClick={deleteDone}>Delete Done</Button>
     </div>
   );
 }
